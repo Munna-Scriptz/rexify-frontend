6ca98057542aa0a02768dfa188fd85689da278dc
Munna-Scripts <munnascriptz@gmail.com>
2026-01-11 22:58:04 +0600

    Navbar 30% Complete and container fixed

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import logo from '../../../public/Logo.png'
+import { NavLink } from 'react-router'
 const Navbar = () => {
   return (
     <>
-        <nav>
-            
+        <nav className='bg-black py-3'> 
+            <div className="container">
+                <div id='Navbar-Row' className='flex items-center justify-between text-white'>
+                    {/* ------------ Image  */}
+                    <div className='w-[120px]'><img src={logo} alt="Rexify Logo" /></div>
+
+                    {/* ------------ NavLinks  */}
+                    <div>
+                        <NavLink to={'/'}>Home</NavLink>
+                    </div>
+
+                    {/* ------------ NavButtons  */}
+                    <div>
+
+                    </div>
+
+                </div>
+            </div>
         </nav>
     </>
   )
